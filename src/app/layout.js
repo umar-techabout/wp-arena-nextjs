958b4dc66305b35f39f7a0790550d4a866b1baf1
umar-techabout <umartechabout@gmail.com>
2024-09-11 12:02:45 +0500

    head tag changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head> 
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Header />
         <ServicesBar />
